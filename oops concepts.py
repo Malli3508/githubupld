f1296d76fd86281674731d0a5f2df4b24ccfc9aa
Malli3508 <115809151+Malli3508@users.noreply.github.com>
2022-11-14 12:38:20 +0530

    threads

--- oops concepts.py
+++ oops concepts.py
@@ -1,21 +1,21 @@
-# finally block
-'''
-# finaly block
-1.finallyblock is a predefined keyword ofif python.
-2.in genral we use finally block with try except block.
-3.one try can have maximum only one finally.
-4.finally block should be after all except if not block.
-5.finally block code will be executes all the time,no matter whether.
-'''
-try:
-    f=open('d:/palleypython.text','r')
-    mylist=f.readlines()
-    print(mylist[0])#printing first line
-    f.close()
-except FileNotFoundError:
-    print('file does not exist.ply try later')
-except IndexError:
-    print('invalid postion..')
-finally:
-    print('ths is finally block...')
+#creating multiple threads
+def m1():
+    print('hii')
+def m2():
+    print('bye')
+m1()
+m2()
+from threading import Thread
+class MyThread(Thread):
+    def run(self):
+        print('this is our thread')
+        print(self.getName)
+class A(Thread):
+    def run(self):
+        print('this code is excuted by thread2...')
+m1=MyThread()
+m1.start()
+m2=A()
+m2.start()
+
 
